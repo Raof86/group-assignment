bbdee2e8e3c1765fde82a413dd8417ae4edfddb7
Yaqubi1 <Yaqubi1_1@users.noreply.github.com>
2020-07-16 02:40:55 +0430

    script.js and menu hide function

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    // document.getElementById("navbar-container").style.opacity = 1;
+    document.getElementById("navbar-container").style.top = "0px";
+  } else {
+    // document.getElementById("navbar-container").style.opacity = 0.1;
+    document.getElementById("navbar-container").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
