a1e8af98e1d9deaaf7d984afabb58e15165e3365
Raof86 <67486433+Raof86@users.noreply.github.com>
2020-07-23 20:24:43 +0200

    new changes

--- js/politics.js
+++ js/politics.js
@@ -1,36 +1,36 @@
 /* slider i politics sidan */
 
-startAutoSlidex();
-var myVarx;
+// startAutoSlidex();
+// var myVarx;
 
-function startAutoSlidex(){
-  myVarx = setInterval(function(){ plusSlidesx(1); }, 5000);
-}
+// function startAutoSlidex(){
+//   myVarx = setInterval(function(){ plusSlidesx(1); }, 5000);
+// }
 
-var mySlideIndex = 1;
-myShowSlides(mySlideIndex);
+// var mySlideIndex = 1;
+// myShowSlides(mySlideIndex);
 
-function plusSlidesx(n){
-  myShowSlides(mySlideIndex += n);
-}
-function myCurrentSlide(n){
-  myShowSlides(mySlideIndex = n);
-}
-function myShowSlides(n){
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if(n > slides.length){
-    mySlideIndex = 1;
-  }
-  if(n < 1){
-    mySlideIndex = slides.length;
-  }
-  for(i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+// function plusSlidesx(n){
+//   myShowSlides(mySlideIndex += n);
+// }
+// function myCurrentSlide(n){
+//   myShowSlides(mySlideIndex = n);
+// }
+// function myShowSlides(n){
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   if(n > slides.length){
+//     mySlideIndex = 1;
+//   }
+//   if(n < 1){
+//     mySlideIndex = slides.length;
+//   }
+//   for(i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
 
-  slides[mySlideIndex - 1].style.display = "block";
-}
+//   slides[mySlideIndex - 1].style.display = "block";
+// }
 
 
 
